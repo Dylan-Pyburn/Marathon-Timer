bb83c59f3285b2953e91eab3fd48cf4c4b870325
Dylan <45494441+Dylan-Pyburn@users.noreply.github.com>
2026-02-25 22:47:50 +0900

    added function skeletons for gui element commands

--- src/gui.py
+++ src/gui.py
@@ -12,7 +12,7 @@ class GUI:
         self.configure_gui()
 
     #=============================================
-    #       UI Elements
+    #      Main UI Elements
     #=============================================
 
     def configure_gui(self):
@@ -33,24 +33,6 @@ class GUI:
         self.tabControl = ttk.Notebook(self.root)
         self.tabControl.pack(expand=1, fill=BOTH)
 
-    def configure_frameTabRankEntry(self):
-        self.frameAppRankEntry = Frame(self.tabControl, bg='skyblue')
-        self.frameAppRankEntry.pack(expand=1, fill=BOTH)
-        self.tabControl.add(self.frameAppRankEntry, text='順位入力')
-
-        self.configure_frameDataEntry()
-        self.configure_frameDataView()
-
-    def configure_frameTabTimer(self):
-        self.frameAppTimer = Frame(self.tabControl, bg='#fc8f79')
-        self.frameAppTimer.pack(expand=1, fill=BOTH) 
-        self.tabControl.add(self.frameAppTimer, text='タイマー')
-
-    def configure_frameTabStats(self):
-        self.frameAppStats = Frame(self.tabControl, bg='#073f18')
-        self.frameAppStats.pack(expand=1, fill=BOTH)
-        self.tabControl.add(self.frameAppStats, text='合計')
-    
     #== Menu ===================================== 
     
     def configure_menubar(self):
@@ -65,29 +47,39 @@ class GUI:
         self.menuFile = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label='ファイル', menu=self.menuFile)
 
-        self.menuFile.add_command(label='新規',             command=None)
-        self.menuFile.add_command(label='開く',             command=None)
-        self.menuFile.add_command(label='エクスポート',     command=None)
-        self.menuFile.add_command(label='保存',             command=None)
-        self.menuFile.add_command(label='名前を付けて保存', command=None)
+        self.menuFile.add_command(label='新規',             command=self.cmd_menuFile_new)
+        self.menuFile.add_command(label='開く',             command=self.cmd_menuFile_open)
+        self.menuFile.add_command(label='エクスポート',     command=self.cmd_menuFile_export)
+        self.menuFile.add_command(label='保存',             command=self.cmd_menuFile_save)
+        self.menuFile.add_command(label='名前を付けて保存', command=self.cmd_menuFile_saveAs)
         self.menuFile.add_separator()
-        self.menuFile.add_command(label='閉じる',           command=None)
+        self.menuFile.add_command(label='閉じる',           command=self.cmd_menuFile_close)
 
     def configure_menuSettings(self):
         self.menuSettings = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label='設定', menu=self.menuSettings)
 
-        self.menuSettings.add_command(label='言語',         command=None)
-        self.menuSettings.add_command(label='キーマップ',   command=None)
+        self.menuSettings.add_command(label='言語',         command=self.cmd_menuSettings_language)
+        self.menuSettings.add_command(label='キーマップ',   command=self.cmd_menuSettings_keymap)
 
     def configure_menuHelp(self):
         self.menuHelp = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label='ヘルプ', menu=self.menuHelp)
 
-        self.menuHelp.add_command(label='使い方',           command=None)
-        self.menuHelp.add_command(label='Docs',             command=None)
+        self.menuHelp.add_command(label='使い方',           command=self.cmd_menuHelp_howToUse)
+        self.menuHelp.add_command(label='Docs',             command=self.cmd_menuHelp_docs)
 
-    #== Tabs ===== =============================== 
+    #=============================================
+    #      Rank Entry Tab
+    #=============================================
+
+    def configure_frameTabRankEntry(self):
+        self.frameAppRankEntry = Frame(self.tabControl, bg='skyblue')
+        self.frameAppRankEntry.pack(expand=1, fill=BOTH)
+        self.tabControl.add(self.frameAppRankEntry, text='順位入力')
+
+        self.configure_frameDataEntry()
+        self.configure_frameDataView()
 
     #== Data Entry =============================== 
 
@@ -103,7 +95,8 @@ class GUI:
         self.labelStudentRank       = Label(self.frameDataEntry, text='順位')
         self.entryStudentRank       = Entry(self.frameDataEntry)
 
-        self.buttonEnterData        = Button(self.frameDataEntry, text='追加', command=self.action_buttonEnterData)
+        self.buttonSaveChange       = Button(self.frameDataEntry, text='保存', command=self.cmd_buttonSaveChange, state=NORMAL)
+        self.buttonEnterData        = Button(self.frameDataEntry, text='追加', command=self.cmd_buttonEnterData)
 
         self.arrange_frameDataEntry()
 
@@ -117,6 +110,7 @@ class GUI:
         self.labelStudentRank.grid(     row=0,  column=2,   padx=10,    pady=10)
         self.entryStudentRank.grid(     row=1,  column=2,   padx=10,    pady=10)
 
+        self.buttonSaveChange.grid(     row=0,  column=3,   padx=20,    pady=10)
         self.buttonEnterData.grid(      row=1,  column=3,   padx=20,    pady=10)
 
         self.frameDataEntry.pack(side='top', anchor="center", pady= 20)
@@ -125,22 +119,82 @@ class GUI:
 
     def configure_frameDataView(self):
         self.frameDataView = Frame(self.frameAppRankEntry, width=500, height=100, bg="white")
-        self.frameDataView.pack(side='top', anchor='center', pady=20)
-        
-        self.listboxDataView = Listbox(self.frameDataView)
-        self.listboxDataView.pack(side=LEFT, fill=BOTH)
+        self.frameDataView.pack(side='top', anchor='center', expand=1, fill=BOTH, pady=20)
 
+        self.listboxDataView = Listbox(self.frameDataView, width=300)
         self.scrollbarDataView = Scrollbar(self.frameDataView, command=self.listboxDataView.yview)
-        self.scrollbarDataView.pack(side=RIGHT, fill=BOTH)
+        
+        self.scrollbarDataView.pack(side=RIGHT, fill='y')
+        self.listboxDataView.pack(side=LEFT,  expand=1)
         
         self.listboxDataView.config(yscrollcommand = self.scrollbarDataView.set)
 
+    #=============================================
+    #       Timer Tab
+    #=============================================
 
+    def configure_frameTabTimer(self):
+        self.frameAppTimer = Frame(self.tabControl, bg='#fc8f79')
+        self.frameAppTimer.pack(expand=1, fill=BOTH) 
+        self.tabControl.add(self.frameAppTimer, text='タイマー')
+
+    #=============================================
+    #       Stats Tab
+    #=============================================
+
+    def configure_frameTabStats(self):
+        self.frameAppStats = Frame(self.tabControl, bg='#073f18')
+        self.frameAppStats.pack(expand=1, fill=BOTH)
+        self.tabControl.add(self.frameAppStats, text='合計')
+    
     #=============================================
     #       Commands
     #=============================================
 
-    def action_buttonEnterData(self):
+    def cmd_buttonSaveChange(self):
+        self.buttonSaveChange.config(state=DISABLED)
+
+    def cmd_buttonEnterData(self):
+
         self.listboxDataView.insert(END,'hi')
+
+    def cmd_listboxDataView_itemClicked(self):
+        pass
+
+    #== File Menu ================================
+
+    def cmd_menuFile_new(self):
+        pass
+
+    def cmd_menuFile_open(self):
+        pass
+
+    def cmd_menuFile_export(self):
+        pass
+
+    def cmd_menuFile_save(self):
+        pass
+
+    def cmd_menuFile_saveAs(self):
+        pass
+
+    def cmd_menuFile_close(self):
+        pass
+
+    #== Settings Menu ============================
+
+    def cmd_menuSettings_language(self):
+        pass
+
+    def cmd_menuSettings_keymap(self):
+        pass
+
+    #== Help Menu ++++============================
+
+    def cmd_menuHelp_howToUse(self):
+        pass
+
+    def cmd_menuHelp_docs(self):
+        pass
 
 
