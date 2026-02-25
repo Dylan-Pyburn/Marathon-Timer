5a1646a4f67aadcd1c86a9199a882af8c618c013
Pyburn Dylan <dpyburn@ishikawa-c.ed.jp>
2026-02-25 09:40:45 +0900

    Tkinter hello world

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+from tkinter import *
 
+WINDOW_TITLE    = 'マラソン タイマー'
+WINDOW_WIDTH    = 1200
+WINDOW_HEIGHT   = 700
 
 
 if __name__ == '__main__':
 
-    print('hello world')
+    root = Tk()
+    root.title(WINDOW_TITLE)
+    root.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
+    
+    labelHello = Label(root, text='Hello Tkinter World!')
+
+    labelHello.pack()
+
+    root.mainloop()
 
 
